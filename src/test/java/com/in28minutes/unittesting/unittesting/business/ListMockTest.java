368bad9015dc444cb6988dd2adbd99133c3ae0d3
James Zhang <liulfbaby@gmail.com>
2022-08-20 22:09:30 +0800

    Mockito mocking and spying.

--- src/test/java/com/in28minutes/unittesting/unittesting/business/ListMockTest.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/business/ListMockTest.java
@@ -1,17 +1,18 @@
 package com.in28minutes.unittesting.unittesting.business;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class ListMockTest {
 
-    List mock = mock(List.class);
+    List<String> mock = mock(List.class);
 
     @Test
     void size_basic() {
@@ -40,4 +41,68 @@ public class ListMockTest {
         assertEquals("in28Minutes", mock.get(1));
     }
 
+    @Test
+    void verificationBasic() {
+        //SUT
+        String value1 = mock.get(0);
+        String value2 = mock.get(1);
+
+        //Verify
+        verify(mock).get(0);
+        verify(mock, times(2)).get(anyInt());
+        verify(mock, atLeast(1)).get(anyInt());
+        verify(mock, atMost(2)).get(anyInt());
+        verify(mock, never()).get(2);
+    }
+
+    @Test
+    void argumentCapturing() {
+        mock.add("SomeString");
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(mock).add(captor.capture());
+
+        assertEquals("SomeString", captor.getValue());
+    }
+
+    @Test
+    void MultipleArgumentCapturing() {
+        mock.add("SomeString1");
+        mock.add("SomeString2");
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(mock, times(2)).add(captor.capture());
+
+        List<String> allValues = captor.getAllValues();
+        assertEquals("SomeString1", allValues.get(0));
+        assertEquals("SomeString2", allValues.get(1));
+    }
+
+    @Test
+    void mocking() {
+        ArrayList arrayListMock = mock(ArrayList.class);
+        System.out.println("arrayListMock.get(0) = " + arrayListMock.get(0));
+        System.out.println("arrayListMock.size() = " + arrayListMock.size());
+        arrayListMock.add("Test");
+        arrayListMock.add("Test2");
+        System.out.println("arrayListMock.size() = " + arrayListMock.size());
+        when(arrayListMock.size()).thenReturn(5);
+        System.out.println("arrayListMock.size() = " + arrayListMock.size());
+    }
+
+    @Test
+    void spying() {
+        ArrayList arrayListSpy = spy(ArrayList.class);
+        arrayListSpy.add("Test0");
+        System.out.println("arrayListSpy.get(0) = " + arrayListSpy.get(0));
+        System.out.println("arrayListSpy.size() = " + arrayListSpy.size());
+        arrayListSpy.add("Test");
+        arrayListSpy.add("Test2");
+        System.out.println("arrayListSpy.size() = " + arrayListSpy.size());
+        when(arrayListSpy.size()).thenReturn(5);
+        System.out.println("arrayListSpy.size() = " + arrayListSpy.size());
+
+        arrayListSpy.add("Test4");
+        System.out.println("arrayListSpy.size() = " + arrayListSpy.size());
+
+        verify(arrayListSpy).add("Test4");
+    }
 }
