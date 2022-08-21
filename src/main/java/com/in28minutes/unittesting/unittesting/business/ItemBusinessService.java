fb2ef257d653fdaf4951fac251568f14117e55a8
James Zhang <liulfbaby@gmail.com>
2022-08-21 23:46:42 +0800

    Writing unit test for REST service mocking business layer.

---
+++ src/main/java/com/in28minutes/unittesting/unittesting/business/ItemBusinessService.java
@@ -0,0 +1,11 @@
+package com.in28minutes.unittesting.unittesting.business;
+
+import com.in28minutes.unittesting.unittesting.model.Item;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ItemBusinessService {
+    public Item retrieveHardcodedItem() {
+        return new Item(1, "Ball", 10, 100);
+    }
+}
--- src/test/java/com/in28minutes/unittesting/unittesting/controller/ItemControllerTest.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/controller/ItemControllerTest.java
@@ -1,26 +1,33 @@
 package com.in28minutes.unittesting.unittesting.controller;
 
+import com.in28minutes.unittesting.unittesting.business.ItemBusinessService;
+import com.in28minutes.unittesting.unittesting.model.Item;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(value = HelloWorldController.class)
+@WebMvcTest(value = ItemController.class)
 public class ItemControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private ItemBusinessService businessService;
+
     @Test
     void dummyItem_basic() throws Exception {
-        // call GET "/hello"
         RequestBuilder request = MockMvcRequestBuilders
                 .get("/dummy-item")
                 .accept(MediaType.APPLICATION_JSON);
@@ -33,4 +40,18 @@ public class ItemControllerTest {
         //verify "Hello World"
         //assertEquals("Hello World", result.getResponse().getContentAsString());
     }
+
+    @Test
+    void itemFromBusinessService_basic() throws Exception {
+        when(businessService.retrieveHardcodedItem()).thenReturn(new Item(1, "Ball", 10, 100));
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/item-from-business-service")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{id:1,name:Ball,price:10,quantity:100}"))
+                .andReturn();
+    }
+
 }
