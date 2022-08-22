7e448fc6498c44f88601d0920251709d66cde1f1
James Zhang <liulfbaby@gmail.com>
2022-08-22 21:24:47 +0800

    Create item entity and populate data with data.sql.

--- src/main/java/com/in28minutes/unittesting/unittesting/model/Item.java
+++ src/main/java/com/in28minutes/unittesting/unittesting/model/Item.java
@@ -1,25 +1,61 @@
 package com.in28minutes.unittesting.unittesting.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
 public class Item {
+
+    @Id
     private int id;
     private String name;
     private int price;
     private int quantity;
 
-    public int getId() {
-        return id;
+    @Transient
+    private int value;
+
+    public Item(int id, String name, int price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Item() {
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getPrice() {
         return price;
     }
 
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     public int getQuantity() {
         return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 
     @Override
@@ -30,12 +66,5 @@ public class Item {
                 ", price=" + price +
                 ", quantity=" + quantity +
                 '}';
-    }
-
-    public Item(int id, String name, int price, int quantity) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into item(id, name, price, quantity)
+values (10001, 'Item1', 10, 20);
