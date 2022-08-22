3cf7107640f347729a0380774e62b913cc2694de
James Zhang <liulfbaby@gmail.com>
2022-08-22 22:05:35 +0800

    Create a RESTFul service talking to the database.

--- src/main/java/com/in28minutes/unittesting/unittesting/business/ItemBusinessService.java
+++ src/main/java/com/in28minutes/unittesting/unittesting/business/ItemBusinessService.java
@@ -1,11 +1,28 @@
 package com.in28minutes.unittesting.unittesting.business;
 
+import com.in28minutes.unittesting.unittesting.data.ItemRepository;
 import com.in28minutes.unittesting.unittesting.model.Item;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ItemBusinessService {
+
+    @Autowired
+    private ItemRepository repostitory;
+
     public Item retrieveHardcodedItem() {
         return new Item(1, "Ball", 10, 100);
     }
+
+    public List<Item> retrieveAllItems() {
+        List<Item> items = repostitory.findAll();
+        for (Item item : items) {
+            item.setValue(item.getPrice() * item.getQuantity());
+        }
+        return items;
+    }
+
 }
--- src/main/java/com/in28minutes/unittesting/unittesting/controller/ItemController.java
+++ src/main/java/com/in28minutes/unittesting/unittesting/controller/ItemController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ItemController {
 
@@ -20,6 +22,11 @@ public class ItemController {
     @GetMapping("/item-from-business-service")
     public Item itemFromBusinessService() {
         return businessService.retrieveHardcodedItem();
+    }
+
+    @GetMapping("/all-items-from-database")
+    public List<Item> retrieveAllItems() {
+        return businessService.retrieveAllItems();
     }
 
 }
